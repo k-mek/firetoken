aa62c0e9eb7eedfb61e5ae4d497dfd8f98b6adf7
k-mek <136653229+k-mek@users.noreply.github.com>
2023-07-22 23:57:19 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xf560e9fc4534cce4a1e1d3ee2aac91c3dc763e7b";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
